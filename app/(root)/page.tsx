8f2d314a7e487629820ec71d59b259ab7e992d9b
asifalam515 <asibulalam515@gmail.com>
2025-08-05 13:08:45 +0600

    root layout updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Next js</h1>
+      <Button variant="secondary">Click Here</Button>
     </div>
   );
 };
